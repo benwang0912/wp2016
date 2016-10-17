d9a88716c7881efc325aff8863eec8081e64c2d4
benwang0912 <benwang0912@gmail.com>
2016-10-17 22:52:18 +0800

    highlight link when mouse over && change embed page when links clicked && toggle them when parent clicked

--- application.js
+++ application.js
@@ -1,3 +1,28 @@
-//$(document).ready(function() {
-//});
+$(document).ready(function() {
+  //highlight when mouse enter
+ $(".links").on("mouseenter", function(){
+    $(this).removeClass("unhighlighted");
+    $(this).addClass("highlighted");
+ }); 
+  //unhighlight when mouse leave
+ $(".links").on("mouseleave", function(){
+    $(this).removeClass("highlighted");
+    $(this).addClass("unhighlighted");
+ });
+  //reload embed page when link clicked
+ $(".links").on("click", function(){
+    var parent = $(".embededPage").parent();
+    var newElement = "<embed src="+ $(this).data("link") +" class='embededPage'></embed>";
+    $(".embededPage").remove();
+    parent.append(newElement);
+ });
+  //toggle works link
+  $(".works").on("click",function(){
+    $(this).parent().find("ul").toggle(300);
+  });
+  $(".refs").on("click",function(){
+    $(this).parent().find("ul").toggle(300);
+  });
+  
+});
 
